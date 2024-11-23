87d66fc4de70fe941bece757d6790c5f1c41a4db
Edor Kacerja <edorkacerja@gmail.com>
2024-11-22 21:25:18 -0700

    removed jobs

--- src/common/util/constants.js
+++ src/common/util/constants.js
@@ -1,3 +1,3 @@
 
 
-export const API_BASE_URL = import.meta.env.VITE_TURTRACK_MANAGER_SERVER_BASE_URL || 'http://localhost:9999/api/v1';
+export const API_BASE_URL = import.meta.env.VITE_TURTRACK_MANAGER_SERVER_API_V1_URL || 'http://localhost:9999/api/v1';
