0dab149b7233dc7ba18c368598755550882af300
Edor Kacerja <edorkacerja@gmail.com>
2024-11-23 17:21:39 -0700

    use proxy always

--- src/common/util/constants.js
+++ src/common/util/constants.js
@@ -1,7 +1,3 @@
-const isProd = import.meta.env.PROD;
-
-export const API_BASE__API_V1_URL = isProd
-    ? '/api/v1'  // This will use Vercel's proxy in production
-    : import.meta.env.VITE_TURTRACK_MANAGER_SERVER_BASE_URL || 'http://localhost:9999/api/v1';
-
-export const API_BASE_URL = API_BASE__API_V1_URL;
+// Always use the proxy path
+export const API_BASE_URL = '/api/v1';
+export const API_BASE__API_V1_URL = API_BASE_URL;
